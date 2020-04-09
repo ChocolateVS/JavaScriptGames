e40ff8f07001c6dde5aae7ea4dfbc432e221f16f
Rowan Thorley <rghthorley@gmail.com>
2020-04-10 01:06:53 +1200

    yyet

--- assets/js/spread.js
+++ assets/js/spread.js
@@ -9,19 +9,28 @@ var checkbox2 = document.querySelector("input[name=randSize]");
 var checkbox3 = document.querySelector("input[name=randSpeed]");
 
 var sliderNumCircle = document.getElementById("circles");
+var sliderNumCorona = document.getElementById("corona");
 var sliderRadius = document.getElementById("radius");
 var sliderVelocity = document.getElementById("velocity");
 var sliderMaxV = document.getElementById("max_speed");
 var sliderMinV = document.getElementById("min_speed");
 
 var radiusInput = 35;
-var numberCircles = 50;
+var numberCircles = 20;
+var numberCorona = 1;
 sliderRadius.oninput = function() {
  radiusInput = this.value;
 }
 
 sliderNumCircle.oninput = function() {
  numberCircles = this.value;
+ //numberCorona = document.getElementById("corona").value;
+ generate();
+}
+
+sliderNumCorona.oninput = function() {
+ numberCorona = this.value;
+ //numberCircles = document.getElementById("circles").value;
  generate();
 }
 
@@ -47,6 +56,7 @@ var sKey = true;
 
 var menu = true;
 
+var display = true;
 window.addEventListener("keydown", event => {
     if (event.keyCode == 32 || event.keyCode == 13 && menu) {
         startBtn();
@@ -117,27 +127,34 @@ function generate() {
         var type = 0;
         circleArray.push(new DrawCircle(x, y, r, dx, dy, color, index, rRand, dxSet, dySet, type));
     }
-    var index = numberCircles + 1;
-    var rRand = Math.random() * (50 - 1) + 1;
-    var r = 35;
-    var x = Math.random() * (innerWidth - r * 2) + r;
-    var y = Math.random() * (innerHeight - r * 2) + r;
-    var dxSet = 10;
-    var dySet = 10;
-    var dx = 15 * (Math.random() - 0.4);
-    var dy = 15 * (Math.random() - 0.4);
-    var color = "red";
-    var type = 1;
-    circleArray.push(new DrawCircle(x, y, r, dx, dy, color, index, rRand, dxSet, dySet, type));
+    for (i = 0; i < numberCorona; i++){
+        var index = numberCircles + i;
+        var rRand = Math.random() * (50 - 1) + 1;
+        var r = 35;
+        var x = Math.random() * (innerWidth - r * 2) + r;
+        var y = Math.random() * (innerHeight - r * 2) + r;
+        var dxSet = 10;
+        var dySet = 10;
+        var dx = 15 * (Math.random() - 0.4);
+        var dy = 15 * (Math.random() - 0.4);
+        var color = "red";
+        var type = 1;
+        circleArray.push(new DrawCircle(x, y, r, dx, dy, color, index, rRand, dxSet, dySet, type));
+    }
 }
 
 animate(); 
-
+var infected = numberCorona;
 function startBtn() {
     remove = false;
     start();
 }
+
 function start() { 
+    infected = numberCorona;
+    display = true;
+    var totalCircle = parseInt(numberCircles) + parseInt(numberCorona);
+    console.log("Circles: " + numberCircles + ", Corona: " + numberCorona + ", Total: " + totalCircle);
     document.getElementById("canvas").style.display = "block";
     document.getElementById("menu").style.display = "none";
     menu = false;
@@ -153,30 +170,48 @@ function start() {
     
     function MouseDown(event) {
         if (!remove) {
+            console.log(infected);
             var x = event.clientX;
             var y = event.clientY;
-
+            var total = 0;
             circleArray.forEach(element => {
                 if (x > element.x - element.r && x < element.x + element.r && y > element.y - element.r && y < element.y + element.r && element.type == 1) {
-                    bad.push(element.index); 
-                    element.x = 0 - (2 * element.r);
-                    element.y = 0 - (2 * element.r);
-                    score ++;
-                    console.log(score);
-                    if (element.index == 0) {
-                        document.getElementById("restartMenu").style.display = "block";
-                        document.getElementById("score").innerHTML = score;
-                    }
-                    if (score == numberCircles) {
-                        document.getElementById("restartAllMenu").style.display = "block";
-                        document.getElementById("restartMenu").style.display = "none";
-                    }
+                    //bad.push(element.index); 
+                    //element.x = 0 - (2 * element.r);
+                    //element.y = 0 - (2 * element.r);
+                    element.type = 0;
+                    element.color = "white";
+                    infected--;
+                }  
+                
+                if (infected == 0) {
+                    console.log("WIN");
+                    //score = (((circleArray.length - 1) - infected)/circleArray.length) * 100;
+                    document.getElementById("restartMenu").style.display = "block";
+                    document.getElementById("score").innerHTML = '100';
                 }
-            });        
+            });  
         }
     }
 }
 
+/*
+if (total == 0) {
+    score = (((circleArray.length - 1) - infected)/circleArray.length) * 100;
+    document.getElementById("restartMenu").style.display = "block";
+    document.getElementById("score").innerHTML = score;
+}
+if (total == numberCircles) {
+    console.log("SORRY ALL EXISTANCE IS DEAD");
+    document.getElementById("restartAllMenu").style.display = "block";
+    document.getElementById("restartMenu").style.display = "none";
+}
+
+if (element.type == 1){
+    total++;
+    console.log(total);
+}
+*/
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0,0, innerWidth,innerHeight);
@@ -186,7 +221,6 @@ function animate(){
         }
     }
 }
-
 
 function DrawCircle(x, y, r, dx, dy, color, index, rRand, dxSet, dySet, type) {
     this.index = index;
@@ -230,16 +264,26 @@ function DrawCircle(x, y, r, dx, dy, color, index, rRand, dxSet, dySet, type) {
         }
         
         if (this.type == 1) {
-        if (!menu) {
-            circleArray.forEach(element =>{
-                if (element.type == 0) {
-                    if(this.x < element.x + (2 * element.r) && this.x + (2 * this.r) > element.x  && this.y < element.y + (2 * element.r) && this.y + (2 * this.r) > element.y) {
-                        element.type = 1;
-                        element.color = "red";
-                        console.log("Collision");
+            if (!menu) {
+                circleArray.forEach(element =>{
+                    if (element.type == 0) {
+                        if(this.x < element.x + (2 * element.r) && this.x + (2 * this.r) > element.x  && this.y < element.y + (2 * element.r) && this.y + (2 * this.r) > element.y) {
+                            element.type = 1;
+                            element.color = "red";
+                            infected++;
+                            console.log(infected);
+                        }   
                     }
-                }
-            });                 
+                }); 
+            }
+        }
+        if (display) {
+            //console.log(parseInt(numberCircles) + parseInt(numberCorona));
+            if (infected == parseInt(numberCircles) + parseInt(numberCorona)) {
+                console.log("LOSE");
+                document.getElementById("restartAllMenu").style.display = "block";
+                document.getElementById("restartMenu").style.display = "none";
+                display = false;
             }
         }
         this.x += this.dx;
@@ -250,6 +294,8 @@ function DrawCircle(x, y, r, dx, dy, color, index, rRand, dxSet, dySet, type) {
 }
 
 function openMenu() {
+    infected = numberCorona;
+    display = true;
     menu = true;
     score = 0;
     circleArray = [];
@@ -261,14 +307,9 @@ function openMenu() {
     document.getElementById("restartAllMenu").style.display = "none";
 }
 
-function continueGame(){
-    menu = false;
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("restartMenu").style.display = "none";
-    document.getElementById("restartAllMenu").style.display = "none";
-}
-
 function restart(){
+    infected = numberCorona;
+    display = true;
     menu = false;
     score = 0;
     circleArray = [];
